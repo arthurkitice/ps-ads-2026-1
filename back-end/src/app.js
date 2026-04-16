68044348778a4d5640550e0a69cb46ebed4d2651
Arthur Kitice <art.ferreira301@gmail.com>
2026-04-16 15:16:03 +0000

    (16/04) Implementação de autenticação no back-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -20,6 +20,13 @@ app.use(cookieParser())
 
 app.use('/', indexRouter)
 
+/******* MIDDLEWARE DE AUTENTICAÇÃO *******/
+
+
+import authMiddleware from './middleware/auth.js'
+app.use(authMiddleware)
+
+
 /**************** ROTAS *******************/
 
 import customersRoute from './routes/customers.js'
