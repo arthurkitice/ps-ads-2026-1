76b651876a0fe8dde10d2c2b64349255579a3dd1
Arthur Kitice <art.ferreira301@gmail.com>
2026-05-28 13:08:54 +0000

    Alteração de nome da footerbar

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -25,7 +25,7 @@ export default function FooterBar() {
           }
         }}
       >
-        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:arthurkitice@gmail.com">Arthur Kitice</a>
       </Typography>
     </Box>
  </>
